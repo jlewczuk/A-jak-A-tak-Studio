2170312412ad660d08d38d858631128cf904bcad
Jarosław Lewczuk <jar.lewczuk@gmail.com>
2019-10-17 21:35:15 +0200

    input company text and photos

--- js/script.js
+++ js/script.js
@@ -1,14 +1,14 @@
 $( document ).ready(function() {
     $('#wp0').waypoint(function(direction) {
-        $("#nav-logo-white").fadeTo(1000, 0);
+        $("#nav-logo-white").fadeTo(1, 0);
         });
     
     $('#wp1').waypoint(function(direction) {
         if (direction ==='down') {
-            $("#nav-logo-white").fadeTo(1000, 1);
+            $("#nav-logo-white").fadeTo(500, 1);
         }
         else {
-                $("#nav-logo-white").fadeTo(1000, 0);        
+                $("#nav-logo-white").fadeTo(500, 0);        
         }
     },{
     offset: '-100vh'
@@ -16,12 +16,16 @@ $( document ).ready(function() {
 
     $('#wp2').waypoint(function(direction) {
         if (direction ==='down') {
-            $("#nav-logo-white").fadeTo(1000, 0);
+            $("#nav-logo-white").fadeTo(500, 0);
         }
         else {
-            $("#nav-logo-white").fadeTo(1000, 1);        
+            $("#nav-logo-white").fadeTo(500, 1);        
         }
     },{
     offset: '-110vh'
      });
+
+     $('.js--scroll').click(function () {
+        $('html, body').animate({scrollTop: $('.js--section-about').offset().top +55}, 1000);
+    });
 });
