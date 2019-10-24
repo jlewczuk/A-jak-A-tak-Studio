48bbcf29a157d0f85373181aaf69766658cfc4f3
Jarosław Lewczuk <jar.lewczuk@gmail.com>
2019-10-24 11:08:50 +0200

    add RWD

--- js/script.js
+++ js/script.js
@@ -1,14 +1,27 @@
 $( document ).ready(function() {
+
+    var logo_dark = document.getElementById('nav-logo-dark');
+    var logo_white = document.getElementById('nav-logo-white');
+
+    if(window.innerWidth <= 768) {
+        logo_dark.src = 'img/favicon.png';
+        logo_white.src = 'img/logo-white.png';
+        }
+        else {
+            logo_dark.src = 'img/nav-logo.png';
+            logo_white.src = 'img/nav-logo-white.png';
+        }
+    });
+
     $('#wp0').waypoint(function(direction) {
         $("#nav-logo-white").fadeTo(1, 0);
-        });
     
     $('#wp1').waypoint(function(direction) {
         if (direction ==='down') {
             $("#nav-logo-white").fadeTo(500, 1);
         }
         else {
-                $("#nav-logo-white").fadeTo(500, 0);        
+            $("#nav-logo-white").fadeTo(500, 0);        
         }
     },{
     offset: '-100vh'
@@ -19,7 +32,7 @@ $( document ).ready(function() {
             $("#nav-logo-white").fadeTo(500, 0);
         }
         else {
-            $("#nav-logo-white").fadeTo(500, 1);        
+            $("#nav-logo-white").fadeTo(500, 1); 
         }
     },{
     offset: '-110vh'
